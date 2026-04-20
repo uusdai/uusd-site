a0e122fcbe27e4a2f0710444fe98349812396b7c
satoxin <satoxin@users.noreply.github.com>
2026-04-20 06:55:07 +0000

    deploy: uusdai/website@ff59e0825caa0093a45eb95f72b0799da8e6027e

---
+++ assets/cursor-BJ1MZj_r.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-jqXRXfX6.js";import"./index-BtsXkDi4.js";import"./index.es-4I0uenRl.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
