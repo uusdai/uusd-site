5162909c06e7e846b107b792bf0583f33a177eee
satoxin <satoxin@users.noreply.github.com>
2026-09-16 03:38:05 +0000

    deploy: uusdai/website@3fb5c76ea4d708ff397624e4134233c986402e5a

---
+++ assets/chevron-right-DS_rGtDe.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-C39N4f8E.js";import"./index-CmhXxAQX.js";import"./index.es-B1h2rWvR.js";import"./events-DQ172AOg.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronRightSvg};
